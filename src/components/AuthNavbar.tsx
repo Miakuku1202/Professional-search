2affc53548c59aec247ca2de9841850a4e32074f
Krutika1204 <krutikasurvase08@gmail.com>
2025-09-14 15:04:17 +0530

    Initial commit2

--- src/components/AuthNavbar.tsx
+++ src/components/AuthNavbar.tsx
@@ -1,10 +1,10 @@
 import { Link } from "react-router-dom";
-
+import Logo from "../assets/logo.png"; 
 export default function AuthNavbar() {
   return (
     <nav className="flex justify-between items-center px-6 py-4 bg-white shadow-md">
       <div className="flex items-center gap-2">
-        <img src="/src/assets/logo.png" alt="Logo"className="h-12 w-auto object-contain"/>
+        <img src={Logo} alt="Logo"className="h-12 w-auto object-contain"/>
         <span className="font-semibold text-lg">Professional Search</span>
       </div>
       <div>
